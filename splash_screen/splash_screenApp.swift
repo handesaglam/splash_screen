418dc570e6017c9512a217a3c538764ccb0788fb
Hande SAĞLAM <63313545+handesaglam@users.noreply.github.com>
2022-10-20 15:28:45 +0300

    Initial Commit

---
+++ splash_screen/splash_screenApp.swift
@@ -0,0 +1,17 @@
+//
+//  splash_screenApp.swift
+//  splash_screen
+//
+//  Created by Hande Sağlam on 20.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct splash_screenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
